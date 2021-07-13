d956ad624df12aa0123465a4d478232c689a91c3
Jsr5126 <87040201+Jsr5126@users.noreply.github.com>
2021-07-13 11:13:30 -0700

    Update SQL_compAnalysis.sql

--- SQL_compAnalysis.sql
+++ SQL_compAnalysis.sql
@@ -1,4 +1,4 @@
- /* Distinct list of Vendors and count of 450% POs sent via EDI */
+ /* Distinct list of Vendors and count of 450% POs created by 'WF-BATCH' */
 SELECT DISTINCT Vendor, COUNT(`External Delivery ID`)
 FROM tblapi_companalysis0712
 WHERE `Created by` = 'WF-BATCH'
@@ -7,7 +7,7 @@ WHERE `Created by` = 'WF-BATCH'
 GROUP BY Vendor
 ORDER BY COUNT(`External Delivery ID`) DESC
 
- /* Distinct list of SIDs by Vendor sent to the PDC with EDI ASNs */
+ /* Distinct list of External IDs by Vendor of 450% POs created by 'WF-BATCH' */
 SELECT DISTINCT `External Delivery ID` as SID, `Created on`, Vendor, `Created by`, `Ref.doc.`
 FROM tblapi_companalysis0712
 WHERE Vendor = 'S3J'
@@ -16,7 +16,7 @@ WHERE Vendor = 'S3J'
 GROUP BY `External Delivery ID`
 ORDER BY `Created on` ASC
 
- /* Distinct list of SIDs by Vendor sent to the PDC with EDI ASNs sent within current month*/
+ /* Distinct list of External IDs by Vendor of 450% POs created by 'WF-BATCH' created in current month*/
 SELECT DISTINCT `External Delivery ID` as SID, `Created on`, Vendor, `Created by`, `Ref.doc.`
 FROM tblapi_companalysis0712
 WHERE MONTH(`Created on`) = MONTH(CURRENT_DATE())
@@ -26,7 +26,7 @@ WHERE MONTH(`Created on`) = MONTH(CURRENT_DATE())
 GROUP BY `External Delivery ID`
 ORDER BY `Created on` ASC
 
- /* Distinct list of SIDs by Vendor sent to the PDC with EDI ASNs sent since a specific date*/
+ /* Distinct list of External IDs by Vendor of 450% POs created by 'WF-BATCH' created since a specific date*/
 SELECT DISTINCT `External Delivery ID` as SID, DATE_FORMAT(`Created on`, '%m/%d/%Y') as 'Create Date', Vendor, `Created by`, `Ref.doc.`
 FROM tblapi_companalysis0712
 WHERE DATE_FORMAT(`Created on`, '%m/%d/%Y') > '07/1/2021'
@@ -35,7 +35,7 @@ WHERE DATE_FORMAT(`Created on`, '%m/%d/%Y') > '07/1/2021'
 GROUP BY `External Delivery ID`
 ORDER BY `Created on` ASC
 
-/* Distinct list of Vendors and date of last EDI ASN sent to the PDC*/
+/* Distinct list of Vendors and newest date of last created by 'WF-BATCH' 450% PO */
 SELECT DISTINCT `External Delivery ID` as SID, MAX(DATE_FORMAT(`Created on`, '%m/%d/%Y')) as 'Create Date', Vendor, `Created by`, `Ref.doc.`
 FROM tblapi_companalysis0712
 GROUP BY Vendor
